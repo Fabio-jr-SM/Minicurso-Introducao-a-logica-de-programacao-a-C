fd869b7638ebce9d1b9b3a011bbd45f78b4e7d0d
Fabio Junior <91484736+Fabio-jr-SM@users.noreply.github.com>
2023-05-17 14:30:13 -0300

    Create leitura-num-int.c

---
+++ algoritmos-aula-1/leitura-num-int.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int num_int; 
+    
+    printf("Digite um numero inteiro: ");
+    scanf("%d",&num_int);
+    
+    printf("\nO numero digitado é: %d",num_int);    
+    return 0;
+}
