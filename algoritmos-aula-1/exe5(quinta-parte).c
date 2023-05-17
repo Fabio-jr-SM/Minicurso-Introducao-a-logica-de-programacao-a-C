e6eb4880b6566894a2299ba505da383827b150a2
Fabio Junior <91484736+Fabio-jr-SM@users.noreply.github.com>
2023-05-17 14:41:44 -0300

    Create exe5(quinta-parte).c

---
+++ algoritmos-aula-1/exe5(quinta-parte).c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    float num;
+    printf("Digite um numero real: ");
+    scanf("%f",&num);
+    num=num/5;
+    printf("\nA quinta parte do numero é: %f",num);
+    
+    return 0;
+}
