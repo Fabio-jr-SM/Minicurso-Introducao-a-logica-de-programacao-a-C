8f1e9d3fb81476aad8497c43341afd938e805d30
Fabio Junior <91484736+Fabio-jr-SM@users.noreply.github.com>
2023-05-17 15:58:12 -0300

    Create exe8(raizPositivo-quadradoD).c

---
+++ algoritmos-aula-1/exe8(raizPositivo-quadradoD).c
@@ -0,0 +1,20 @@
+
+#include <stdio.h>
+#include <math.h>
+
+
+int main()
+{
+    float a,raiz,quadrado;
+    
+    printf("Digite dois numeros:\n");
+    scanf("%f",&a);
+    
+    if(a>+0){
+        raiz=sqrt(a);
+        printf("Raiz quadrada é: %f",raiz);
+    } else if(a<0){
+        quadrado=pow(a,2);
+        printf("Quadrado do numero é: %f",quadrado);
+    }
+}
