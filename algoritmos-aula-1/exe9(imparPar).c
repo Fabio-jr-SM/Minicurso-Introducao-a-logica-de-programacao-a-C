6e75dfc55970982e0f44e47f8bd9f3fa335e1484
Fabio Junior <91484736+Fabio-jr-SM@users.noreply.github.com>
2023-05-17 16:43:55 -0300

    Create exe9(imparPar).c

---
+++ algoritmos-aula-1/exe9(imparPar).c
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+#include <math.h>
+
+
+int main()
+{
+    int a;
+    
+    printf("Digite um numero:\n");
+    scanf("%d",&a);
+    
+    if(a%2==0){
+        printf("Numero é Par");
+    }else{
+        printf("Numero é impar");
+    }
+}
