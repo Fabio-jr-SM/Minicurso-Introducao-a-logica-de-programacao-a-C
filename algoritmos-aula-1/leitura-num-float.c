e4867b2e66bfaade849b7d9af865897b4a72fde8
Fabio Junior <91484736+Fabio-jr-SM@users.noreply.github.com>
2023-05-17 14:30:48 -0300

    Create leitura-num-float.c

---
+++ algoritmos-aula-1/leitura-num-float.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    float num_float;
+    printf("Digite um numero real: ");
+    scanf("%f",&num_float);
+    
+    printf("\nO numero digitado é: %f",num_float);
+    
+    return 0;
+}
