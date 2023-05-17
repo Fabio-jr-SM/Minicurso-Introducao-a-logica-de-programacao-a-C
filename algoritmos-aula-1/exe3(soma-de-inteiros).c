a8474cdc47a40ff2eeae3be4ae4b1735278f7c5e
Fabio Junior <91484736+Fabio-jr-SM@users.noreply.github.com>
2023-05-17 14:35:45 -0300

    Create exe3(soma-de-inteiros).c

---
+++ algoritmos-aula-1/exe3(soma-de-inteiros).c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int a,b,c,soma;
+    printf("Digite 3 numero inteiros: ");
+    scanf("%d %d %d",&a,&b,&c);
+    soma=a+b+c;
+    printf("\nO numero digitado é: %d",soma);
+    
+    return 0;
+}
