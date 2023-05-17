16fe1bbc233cadba39a3340b4b9170d994bb7eeb
Fabio Junior <91484736+Fabio-jr-SM@users.noreply.github.com>
2023-05-17 14:39:39 -0300

    Create exe4(num-quadrado).c

---
+++ algoritmos-aula-1/exe4(num-quadrado).c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    float num;
+    printf("Digite um numero inteiro: ");
+    scanf("%f",&num);
+    num=num*num;
+    printf("\nO numero ao quadrado é: %f",num);
+    
+    return 0;
+}
